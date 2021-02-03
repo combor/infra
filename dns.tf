0c5c50124e3098a48811e5815b4872602858a9ca
Piotr Komborski <piotr.komborski@gmail.com>
2021-02-03 21:57:59 +0000

    Provision unify A record

--- dns.tf
+++ dns.tf
@@ -1,3 +1,11 @@
 resource "aws_route53_zone" "kombor-ski" {
   name = "kombor.ski"
 }
+
+resource "aws_route53_record" "unifi" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "unifi.kombor.ski"
+  type    = "A"
+  ttl     = "3600"
+  records = [aws_eip.worker.public_ip]
+}
