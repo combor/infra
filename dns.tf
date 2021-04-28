f571a17f61498c9356813eb7fee711b3aec7d830
Piotr Komborski <piotr.komborski@gmail.com>
2021-04-28 01:14:22 +0100

    unifi PTR
    and portal record

--- dns.tf
+++ dns.tf
@@ -9,3 +9,19 @@ resource "aws_route53_record" "unifi" {
   ttl     = "3600"
   records = ["172.31.255.2"]
 }
+
+resource "aws_route53_record" "unifi_reverse" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "172.31.255.2"
+  type    = "PTR"
+  ttl     = "3600"
+  records = ["portal.kombor.ski"]
+}
+
+resource "aws_route53_record" "portal" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "portal.kombor.ski"
+  type    = "A"
+  ttl     = "3600"
+  records = ["172.31.255.2"]
+}
