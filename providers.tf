d841a7b7eb30f91211521dd1c3063b6e65663142
Piotr Komborski <piotr.komborski@gmail.com>
2022-02-23 11:17:19 +0000

    Remove leftovers

--- providers.tf
+++ providers.tf
@@ -1,14 +1,3 @@
-provider "ct" {}
-
 provider "aws" {
   region = var.aws_region
-}
-
-terraform {
-  required_providers {
-    ct = {
-      source  = "poseidon/ct"
-      version = "0.10.0"
-    }
-  }
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "name" {
-  type        = string
-  description = "name of your infrastructure"
-  default     = "infra"
-}
-
-variable "host_cidr" {
-  type        = string
-  description = "CIDR IPv4 range"
-  default     = "10.0.0.0/16"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS region"
