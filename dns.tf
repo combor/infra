d5a20d02bcc7f09d6c45f2145063a12ff7412ba7
Piotr Komborski <piotr.komborski@gmail.com>
2022-11-10 11:42:00 +0000

    Add bunch of proton DNS records

--- dns.tf
+++ dns.tf
@@ -33,3 +33,51 @@ resource "aws_route53_record" "proton_verification" {
   ttl     = "3600"
   records = ["protonmail-verification=3ab66f95a1d68e13eb6e4cb285c8075bccd998db"]
 }
+
+resource "aws_route53_record" "proton_spf" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "kombor.ski"
+  type    = "TXT"
+  ttl     = "3600"
+  records = ["v=spf1 include:_spf.protonmail.ch mx ~all"]
+}
+
+resource "aws_route53_record" "proton_dkim" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "protonmail._domainkey.kombor.ski"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["protonmail.domainkey.dbjmzzyfyzyrvfq7cfoza4htlq44zbjkph6eu6zqezt7xx3bgaoxa.domains.proton.ch"]
+}
+
+resource "aws_route53_record" "proton_dkim2" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "protonmail2._domainkey.kombor.ski"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["protonmail2.domainkey.dbjmzzyfyzyrvfq7cfoza4htlq44zbjkph6eu6zqezt7xx3bgaoxa.domains.proton.ch"]
+}
+
+resource "aws_route53_record" "proton_dkim3" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "protonmail3._domainkey.kombor.ski"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["protonmail3.domainkey.dbjmzzyfyzyrvfq7cfoza4htlq44zbjkph6eu6zqezt7xx3bgaoxa.domains.proton.ch"]
+}
+
+resource "aws_route53_record" "proton_dmarc" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "_dmarc.kombor.ski"
+  type    = "TXT"
+  ttl     = "3600"
+  records = ["v=DMARC1; p=none"]
+}
+
+resource "aws_route53_record" "proton_mx" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "kombor.ski"
+  type    = "MX"
+  ttl     = "3600"
+  records = ["10 mail.protonmail.ch", "20 mailsec.protonmail.ch"]
+}
