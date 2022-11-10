fc470ae1955ef190252044d1326f3f3815e01269
Piotr Komborski <piotr.komborski@gmail.com>
2022-11-10 10:56:10 +0000

    Proton verification

--- dns.tf
+++ dns.tf
@@ -25,3 +25,11 @@ resource "aws_route53_record" "caa" {
   ttl     = "3600"
   records = ["0 issue \"letsencrypt.org\""]
 }
+
+resource "aws_route53_record" "proton_verification" {
+  zone_id = aws_route53_zone.kombor-ski.zone_id
+  name    = "@"
+  type    = "TXT"
+  ttl     = "3600"
+  records = ["protonmail-verification=3ab66f95a1d68e13eb6e4cb285c8075bccd998db"]
+}
