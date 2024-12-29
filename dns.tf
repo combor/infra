eb234e375599850a63568cca10c11623a65f03f5
Piotr Komborski <163394+combor@users.noreply.github.com>
2024-12-29 20:47:04 +0000

    Remove pihole record
    no longer used

--- dns.tf
+++ dns.tf
@@ -10,14 +10,6 @@ resource "aws_route53_record" "unifi" {
   records = ["ciepla-zupa.chickenkiller.com"]
 }
 
-resource "aws_route53_record" "pihole" {
-  zone_id = aws_route53_zone.kombor-ski.zone_id
-  name    = "pihole.kombor.ski"
-  type    = "CNAME"
-  ttl     = "3600"
-  records = ["ciepla-zupa.chickenkiller.com"]
-}
-
 resource "aws_route53_record" "caa" {
   zone_id = aws_route53_zone.kombor-ski.zone_id
   name    = "kombor.ski"
